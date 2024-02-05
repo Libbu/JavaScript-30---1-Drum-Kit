3874a862a7834610df6acb9f29565b183c4f008e
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-02-05 16:51:44 +0000

    drumkit completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-window.addEventListener('keydown',function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
-});
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stop the function from running altogether
+    audio.currentTime = 0 ; //rewind to start
+    audio.play();
+    key.classList.add('playing');
+};
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip it if its not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
